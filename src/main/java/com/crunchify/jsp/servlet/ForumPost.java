f2f18ce2ef1687907c8cc1376e64e5785537f9ba
Carlos Daniel Bayona <carlosdanielbayo@gmail.com>
2019-01-31 18:33:17 -0500

    clean

---
+++ src/main/java/com/crunchify/jsp/servlet/ForumPost.java
@@ -0,0 +1,24 @@
+package com.crunchify.jsp.servlet;
+
+
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet(name = "ForumPost")
+public class ForumPost extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
